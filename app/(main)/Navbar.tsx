851def531497685199af6a22121855d6f4e79c06
Quentin Liu <74765847+yepQuentin@users.noreply.github.com>
2025-02-17 14:40:27 +0800

    update ico&svg

--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ThemeToggle } from "@/components/ThemeToggle";
-import { Leaf } from "lucide-react";
+import { Beef } from "lucide-react";
 
 export default function Navbar() {
   const handleLogoClick = () => {
@@ -13,7 +13,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 flex items-center justify-between border-foreground/10 border-b px-4 py-2">
       <div onClick={handleLogoClick}>
-        <Leaf className="cursor-pointer dark:invert" />
+        <Beef className="cursor-pointer dark:invert" />
       </div>
       <ThemeToggle />
     </nav>
