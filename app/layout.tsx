0700d2acafd6eb12d156663d7b9b4aa9f7962b1c
Quentin <74765847+yepQuentin@users.noreply.github.com>
2025-06-11 09:04:45 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
           <Toaster />
         </ThemeProvider>
